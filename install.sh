c4d1f1b224034eedb2e37e03014af62c7b07b4e5
Matthew Signorini <matthew.signorini@gmail.com>
2014-01-29 13:05:52 +1100

    Added install and uninstall scripts, and also a script to remove files that are generated automatically by inoscope.
    Also added a gitignore file, so that git will be safe around vim's swap
    files.

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!  /bin/bash
+#
+#   Script to install the inoscope scripts to the appropriate locations.
+
+. config
+
+install -m 0755 inoscope ${PREFIX}/bin
+install -m 0755 inoclean ${PREFIX}/bin
+
+# vim: ft=sh ts=4 sw=4 et
---
+++ uninstall.sh
@@ -0,0 +1,9 @@
+#!  /bin/bash
+#
+#   Uninstall the inoscope scripts.
+
+. config
+
+rm ${PREFIX}/bin/inoscope ${PREFIX}/bin/inoclean
+
+# vim: ft=sh ts=4 sw=4 et
